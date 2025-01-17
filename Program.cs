4e4ccfcbf89fb4055953682a9f1e7e09b74edaca
Mohamed Aziz Sghaier <mohamedaziz.sghaier@supcom.tn>
2025-01-17 12:41:56 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Add Portfolio route
+app.MapControllerRoute(
+    name: "portfolio",
+    pattern: "{controller=Portfolio}/{action=Index}/{id?}");
+
+app.Run();
